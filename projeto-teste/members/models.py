dacadbdcf1410835955a462344d428d839aa7a09
klyrapt <klyra.pt@gmail.com>
2025-08-10 20:17:23 +0100

    feat add :  register model in admin

--- projeto-teste/members/models.py
+++ projeto-teste/members/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Members(models.Model):
     firstname = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200)
+    phone = models.IntegerField(null=True)
+    data_joined = models.DateField(null=True)
+
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+    
--- projeto-teste/members/views.py
+++ projeto-teste/members/views.py
@@ -1,7 +1,7 @@
 
 from django.template import loader
 from django.http import HttpResponse
-
+from .models import Members
 
 
 # Create your views here.
@@ -9,7 +9,25 @@ from django.http import HttpResponse
 
 
 def members(request):
-    template = loader.get_template('myfirst.html')
+    members_list = Members.objects.all()
+    template = loader.get_template('member_list.html')
+    context = {
+        "all_members": members_list
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, id):
+    member_detail = Members.objects.get(id=id)
+    template = loader.get_template("member_detail.html")
+
+    context = {
+        "member_detail": member_detail
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def main(request):
+    template = loader.get_template("main.html")
+
     return HttpResponse(template.render())
-
-
